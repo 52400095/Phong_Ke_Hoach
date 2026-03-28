7bcf1ba4d5456f5dd509ec84fb7556daaca5a474
52400095 <52400095@student.tdtu.edu.vn>
2026-03-28 22:41:07 +0700

    Add Contract data class

---
+++ Models/Contract.cs
@@ -0,0 +1,8 @@
+public class Contract
+{
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
