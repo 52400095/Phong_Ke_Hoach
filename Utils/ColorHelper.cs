4ab7a8cc9b411701fa550a4016fece82204bf978
52400095 <52400095@student.tdtu.edu.vn>
2026-03-28 22:40:55 +0700

    Create ColorHelper.cs file with color definitions

---
+++ Utils/ColorHelper.cs
@@ -0,0 +1,26 @@
+// ColorHelper.cs
+
+using System;
+using System.Collections.Generic;
+
+namespace Utils
+{
+    public static class ColorHelper
+    {
+        public static Dictionary<string, string> Colors = new Dictionary<string, string>
+        {
+            { "Red", "#FF0000" },
+            { "Green", "#00FF00" },
+            { "Blue", "#0000FF" },
+            { "Black", "#000000" },
+            { "White", "#FFFFFF" },
+            { "Yellow", "#FFFF00" },
+            { "Cyan", "#00FFFF" },
+            { "Magenta", "#FF00FF" },
+            { "Gray", "#808080" },
+            { "Orange", "#FFA500" },
+            { "Purple", "#800080" },
+            { "Pink", "#FFC0CB" },
+        };
+    }
+}
