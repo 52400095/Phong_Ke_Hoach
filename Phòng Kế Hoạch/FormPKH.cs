3fc511864b67096da395b8674fbace8752d2bffe
52400095 <52400095@student.tdtu.edu.vn>
2026-03-28 22:36:15 +0700

    Add files via upload

---
+++ Phòng Kế Hoạch/FormPKH.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phòng_Kế_Hoạch
+{
+    public partial class FormPKH : Form
+    {
+        public FormPKH()
+        {
+            InitializeComponent();
+        }
+
+        private void lblTenNV_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void thanhngang2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlContainer_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void labelNameApp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTienDoHopDong_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLichSuPhanCong_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTiepNhanDonHang_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThietLapMau_Click(object sender, EventArgs e)
+        {
+            // 1. Xóa các giao diện cũ đang hiển thị trong Panel (nếu có)
+            panelcontent.Controls.Clear();
+
+            // 2. Khởi tạo giao diện User Control Thiết Lập Mẫu (Cái bạn vừa tạo ở Bước 1)
+            UCThietLapMau ucThietLap = new UCThietLapMau();
+
+            // 3. Cài đặt để nó phóng to lấp đầy toàn bộ cái Panel
+            ucThietLap.Dock = DockStyle.Fill;
+
+            // 4. Thêm nó vào Panel để hiển thị lên màn hình (KHÔNG nhét nút vào)
+            panelcontent.Controls.Add(ucThietLap);
+        }
+
+        private void FormPKH_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Phòng Kế Hoạch/UCThietLapMau.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phòng_Kế_Hoạch
+{
+    public partial class UCThietLapMau : UserControl
+    {
+        public UCThietLapMau()
+        {
+            InitializeComponent();
+            btnThemNenMau.Enabled = false;
+            cboxHopDong.Enabled = false;
+            LoadComboBox();
+
+            cboxCongTy.SelectedIndexChanged += (s, e) =>
+            {
+                if (cboxCongTy.SelectedIndex > 0)
+                {
+                    cboxHopDong.Enabled = true;
+                    cboxHopDong.Items.Clear();
+                    cboxHopDong.Items.Add("-- Chọn hợp đồng --");
+
+                    if (cboxCongTy.SelectedItem.ToString() == "Nhà Máy Phú Lộc")
+                    {
+                        cboxHopDong.Items.Add("HĐ-001");
+                        cboxHopDong.Items.Add("HĐ-002");
+                    }
+                    else if (cboxCongTy.SelectedItem.ToString() == "KCN Tân Tạo")
+                    {
+                        cboxHopDong.Items.Add("HĐ-003");
+                        cboxHopDong.Items.Add("HĐ-004");
+                    }
+                    else if (cboxCongTy.SelectedItem.ToString() == "Sông Hương")
+                    {
+                        cboxHopDong.Items.Add("HĐ-005");
+                        cboxHopDong.Items.Add("HĐ-006");
+                    }
+
+                    cboxHopDong.SelectedIndex = 0;
+                }
+                else
+                {
+                    cboxHopDong.Enabled = false;
+                    cboxHopDong.Items.Clear();
+                    cboxHopDong.Items.Add("-- Chọn hợp đồng --");
+                    cboxHopDong.SelectedIndex = 0;
+                }
+                KiemTraDuDieuKien();
+            };
+
+            cboxHopDong.SelectedIndexChanged += (s, e) => KiemTraDuDieuKien();
+        }
+
+        private void KiemTraDuDieuKien()
+        {
+            btnThemNenMau.Enabled = cboxCongTy.SelectedIndex > 0
+                                  && cboxHopDong.SelectedIndex > 0;
+        }
+
+        private void LoadComboBox()
+        {
+            cboxCongTy.Items.Clear();
+            cboxCongTy.Items.Add("-- Chọn công ty --");
+            cboxCongTy.Items.Add("Nhà Máy Phú Lộc");
+            cboxCongTy.Items.Add("KCN Tân Tạo");
+            cboxCongTy.Items.Add("Sông Hương");
+            cboxCongTy.SelectedIndex = 0;
+
+            cboxHopDong.Items.Clear();
+            cboxHopDong.Items.Add("-- Chọn hợp đồng --");
+            cboxHopDong.SelectedIndex = 0;
+        }
+
+        private void btnThemNenMau_Click(object sender, EventArgs e)
+        {
+            using (var form = new FormThemNenMau())
+            {
+                form.ShowDialog(this.FindForm());
+            }
+        }
+    }
+}
